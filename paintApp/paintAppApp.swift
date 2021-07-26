6799fdad2b6f5415ecb8f5d232abde36f3e526a5
41nyaa <sny.cho@gmail.com>
2021-07-26 21:07:12 +0900

    Initial Commit

---
+++ paintApp/paintAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  paintAppApp.swift
+//  paintApp
+//
+//  Created by scho on 2021/07/26.
+//
+
+import SwiftUI
+
+@main
+struct paintAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
